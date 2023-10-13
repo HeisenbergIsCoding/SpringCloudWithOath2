130ae266456601916dd8f8cf66dedad14d1bc083
Ziv Chan <zivchan.tw@gmail.com>
2023-10-13 20:56:46 +0800

    init: first commit

---
+++ src/main/java/com/heisenberg/springcloudwithoath2/SpringCloudWithOath2Application.java
@@ -0,0 +1,13 @@
+package com.heisenberg.springcloudwithoath2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudWithOath2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudWithOath2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/heisenberg/springcloudwithoath2/SpringCloudWithOath2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heisenberg.springcloudwithoath2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudWithOath2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
